2af800507bd9cc86d440ab0c02cf7b5488e16a08
KatiaSonally <katiasonally@yahoo.com.br>
2023-11-19 20:46:47 -0300

    feat: establishes JS goals

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+/*
+Objetivo 1 - Quando o usuário clicar no botão de 'Veja o trailer', devemos abrir a modal com o vídeo do trailer.
+- Passo 1 - Pegar o elemento que representa o botão na tela, usando o JS.
+- Passo 2 - Identificar quando o usuário clicar no botão.
+- Passo 3 - Pegar o elemento da modal no JS.
+- Passo 4 - Abrir a modal na tela.
+
+Objetivo 2 - Quando o usuário clicar no X devemos fechar a modal.
+- Passo 2 - Pegar o elemento de fechar a modal, usando o JS.
+- Passo 3 - Identificar quando o usuário clicar no X.
+- Passo 4 - Fechar a modal.
+*/
+
