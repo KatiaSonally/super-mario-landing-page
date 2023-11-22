a54c70cf2a51c35aacc801305e82ed000c8e1e4c
KatiaSonally <katiasonally@yahoo.com.br>
2023-11-21 23:02:37 -0300

    feat: finishes JS

--- src/js/index.js
+++ src/js/index.js
@@ -11,31 +11,22 @@ Objetivo 2 - Quando o usuário clicar no X devemos fechar a modal.
 - Passo 2.3 - Fechar a modal.
 */
 
-//Objetivo 1 - Quando o usuário clicar no botão de 'Veja o trailer', devemos abrir a modal com o vídeo do trailer.
-//- Passo 1.1 - Pegar o elemento que representa o botão na tela, usando o JS.
 const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
 const video = document.getElementById("video");
-
-//- Passo 1.3 - Pegar o elemento da modal no JS.
 const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
 
-const linkDoVideo = "https://www.youtube.com/embed/Cb4WV4aXBpk";
+function alternarModal() {
+    modal.classList.toggle("aberto");
+}
 
-//- Passo 1 .2- Identificar quando o usuário clicar no botão.
 botaoTrailer.addEventListener("click", () => {
-    //- Passo 1.4 - Abrir a modal na tela.
-    modal.classList.add("aberto");
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
 });
 
-//Objetivo 2 - Quando o usuário clicar no X devemos fechar a modal.
-//- Passo 2.1 - Pegar o elemento de fechar a modal, usando o JS.
-const botaoFecharModal = document.querySelector(".fechar-modal");
-
-//- Passo 2.2 - Identificar quando o usuário clicar no X.
 botaoFecharModal.addEventListener("click", () => {
-    //- Passo 2.3 - Fechar a modal.
-    modal.classList.remove("aberto");
+    alternarModal();
     video.setAttribute("src", "");
 })
-
-
